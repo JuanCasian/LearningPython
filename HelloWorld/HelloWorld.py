24870f8f01cff30696bc960fd7084518831a8e2e
Juan Casian <juanpedrocasian@gmail.com>
2018-09-06 22:40:13 -0700

    Adding Hello World script

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello World!");
+
+# this is setting checking the property of name of the program,
+# This if is checked to see if we are running this file or 
+# if it is being imported to another library.
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning Python Course
+In this repo you can find several excercices done int eh Lynda Course "Learning Python". This has beginners excercises to learn the basics of python.
